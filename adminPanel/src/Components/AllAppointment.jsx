bcd87a43530eedfd0317edc6cc631e43bbce4e0b
Guni paliwal <gpaliwal2605@gmail.com>
2025-02-22 16:06:50 +0530

    changes

--- adminPanel/src/Components/AllAppointment.jsx
+++ adminPanel/src/Components/AllAppointment.jsx
@@ -1,8 +1,9 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AdminContext } from "../store/AdminContext";
 import { toast } from "react-toastify";
 import axios from "axios";
 const AllAppointment = () => {
+  const [appointments, setAppointments] = useState([]);
   const { backendUrl, atoken } = useContext(AdminContext);
   useEffect(() => {
     const fetchAppointment = async () => {
@@ -10,7 +11,8 @@ const AllAppointment = () => {
         const api = backendUrl + "/api/admin/all/appointments";
         console.log(api);
         const response = await axios.get(api, { headers: { atoken } });
-        console.log(response.data);
+        console.log(response.data.appointments);
+        setAppointments(response.data.appointments);
       } catch (error) {
         toast.error(error.message);
       }
@@ -18,8 +20,32 @@ const AllAppointment = () => {
     fetchAppointment();
   }, []);
   return (
-    <div>
-      <h1>All appointment</h1>
+    <div className="w-[80vw]">
+      <h1 className="font-semibold text-2xl">All appointment</h1>
+      <div className="flex-col mx-10">
+        <div className="flex justify-evenly text-2xl p-2 border border-zinc-700 rounded-md my-10">
+          <p>Doctor Name</p>
+          <p>UserName</p>
+          <p>Slot Date and Time</p>
+          <p>Amount</p>
+          <p>Status</p>
+        </div>
+        {appointments &&
+          appointments.map((item) => (
+            <div
+              key={item._id}
+              className="flex justify-evenly p-2 border border-zinc-700 rounded-md "
+            >
+              <p>{item.docData.name}</p>
+              <p>{item.userData.name}</p>
+              <p>
+                {item.slotDate} at {item.slotTime}
+              </p>
+              <p>{item.amount}</p>
+              <p>{item.cancelled ? "cancelled" : "pending"}</p>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
